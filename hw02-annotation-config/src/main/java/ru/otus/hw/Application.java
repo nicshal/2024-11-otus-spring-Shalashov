31da58c22ada5281b8ef501cca9ec896fb8e81fb
Nikolay Shalashov <nicshal@mail.ru>
2025-01-08 17:13:04 +0300

    hw02: готовое современное приложение на чистом Spring. Исправления по результатам code review

--- hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
@@ -3,9 +3,11 @@ package ru.otus.hw;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerService;
 
 @ComponentScan
+@PropertySource(value = "classpath:application.properties")
 @Configuration
 public class Application {
 
